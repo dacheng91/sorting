c9d785a9d98adfe0280d38645736415f77705f86
陈诚 <247417366@qq.com>
2016-06-19 00:27:38 +0800

    test

---
+++ sorting/src/sorting/TestBubbleSort.java
@@ -0,0 +1,47 @@
+package sorting;
+
+import org.junit.Test;
+
+public class TestBubbleSort {
+	
+	// 冒泡排序思想：从右到左，两两比较，左边大于右边，则交换位置
+	// 若有n个元素，进行n-1趟循环，第一趟循环进行n-1次比较，以后每趟逐级递减。若该趟循环中没有发生位置交换，跳出循环
+
+	public void bubbleSort(int arr[], int n) {
+		// 进行n-1趟循环
+		for (int i = 0; i < n - 1; i++) {
+			// 定义是否交换位置的标记
+			boolean isSwap = false;
+			// 最初n-1次比较，然后逐级递减，但是每次都要从第一个元素开始比较。
+			for (int j = 0; j < n - 1-i; j++) {
+				// 如果左边元素大于右边元素，那么交换元素位置，发生位置交换改变标记
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j + 1];
+					arr[j + 1] = arr[j];
+					arr[j] = temp;
+					isSwap = true;
+				}	
+			}
+			// 如果本趟循环没有发生位置交换，那么退出循环。
+			if (isSwap == false) {
+				break;
+			}
+//			打印出每一趟排序后的顺序
+			printArr(arr,12);
+			System.out.println();
+		}
+	}
+
+	public void printArr(int arr[], int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i]+",");
+		}
+	}
+	
+	@Test
+	public void testBubbleSort(){
+		int arr[]={23,45,13,44,67,2,5,7,15,1,99,88};
+		bubbleSort(arr,12);
+//		printArr(arr,12);
+	}
+}
---
+++ sorting/src/sorting/TestSelectSorting.java
@@ -0,0 +1,42 @@
+package sorting;
+
+import org.junit.Test;
+
+public class TestSelectSorting {
+	
+	public void selectSort(int arr[]){
+//		外循环趟数为arr.length-1次
+		for (int i = 0; i < arr.length-1; i++) {
+//			把第一个未排序元素设为最小元素
+			int minIndex=i;
+//			内循环，从下一个未排序元素开始循环到最后一个未排序元素
+			for (int j = i+1; j < arr.length; j++) {
+				if(arr[j]<arr[minIndex]){
+					minIndex=j;
+				}
+			}
+//			内循环结束后，把当前第一个元素与本趟循环中最小的元素交换位置。
+			int temp=arr[i];
+			arr[i]=arr[minIndex];
+			arr[minIndex]=temp;
+			
+			printArr(arr);
+			System.out.println();
+		}
+	}
+	
+	public void printArr(int arr[]){
+//		循环数组中的每一个元素，并打印出来
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+",");
+		}
+	}
+	
+	@Test
+	public void testSelectSort(){
+		int arr[]={45,34,77,12,3,5,7};
+		selectSort(arr);
+		
+	}
+	
+}
