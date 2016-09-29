538ccbd5fc017e88106ee62d5b99515277e5de5f
chencheng <247417366@qq.com>
2016-09-29 16:51:43 +0800

    update

--- sorting/src/sorting/TestBubbleSort.java
+++ sorting/src/sorting/TestBubbleSort.java
@@ -7,7 +7,7 @@ public class TestBubbleSort {
 	// 冒泡排序思想：从右到左，两两比较，左边大于右边，则交换位置
 	// 若有n个元素，进行n-1趟循环，第一趟循环进行n-1次比较，以后每趟逐级递减。若该趟循环中没有发生位置交换，跳出循环
 
-	public void willBeConflicting(int arr[], int n) {
+	public void justMeUpdate(int arr[], int n) {
 		// 进行n-1趟循环
 	
 		sysout("就我一个人增加啊！！！！！！！！！！")
